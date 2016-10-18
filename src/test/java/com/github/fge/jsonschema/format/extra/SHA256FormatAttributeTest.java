a2bd34cecc310915a3da0c7a0371d58c175bef0a
jackiezjiang <jackiezjiang@gmail.com>
2016-10-18 17:41:41 -0400

    Add files via upload

---
+++ src/test/java/com/github/fge/jsonschema/format/extra/SHA256FormatAttributeTest.java
@@ -0,0 +1,16 @@
+
+package com.github.fge.jsonschema.format.extra;
+
+import java.io.IOException;
+
+public final class SHA1FormatAttributeTest
+    extends ExtraFormatAttributeTest
+{
+    public SHA256FormatAttributeTest()
+        throws IOException
+    {
+        super("sha256");
+    }
+}
+
+
