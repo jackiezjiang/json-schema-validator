ddc4546e2c9a3f413d68290977938148e470027a
jackiezjiang <jackiezjiang@gmail.com>
2016-10-19 10:06:29 -0400

    Update Example1Test.java

--- src/test/java/com/github/fge/jsonschema/example/Example1Test.java
+++ src/test/java/com/github/fge/jsonschema/example/Example1Test.java
@@ -2,19 +2,14 @@
 package com.github.fge.jsonschema.examples;
 import java.io.IOException;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
-public  final class Example1Test
+public  class Example1Test
 
 {
 
-    
-  
- public static void main(final String... args)
+ public static void main(String[] args)
         throws IOException, ProcessingException {
             Example1.main(new String[0]);
         }
-
-
-
 }
 
 
