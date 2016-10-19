25c56c4d3dab151ad5f94c0ea74d54fccdf79bde
jackiezjiang <jackiezjiang@gmail.com>
2016-10-19 00:10:32 -0400

    Update Example1Test.java

--- src/test/java/com/github/fge/jsonschema/example/Example1Test.java
+++ src/test/java/com/github/fge/jsonschema/example/Example1Test.java
@@ -2,14 +2,22 @@
 package com.github.fge.jsonschema.examples;
 import java.io.IOException;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
-public final class Example1Test
+public  class Example1Test
 
 {
 
-    public static void main(String[] args){
-        Example1.main(args);
+     public void call(){
+       Example1.main(null);
     }
-
+    
+    
+  
+ public static void main(final String... args)
+        throws IOException, ProcessingException {
+            Example1Test e1 = new Example1Test();
+            e1.call();
+            
+        }
 
 
 
