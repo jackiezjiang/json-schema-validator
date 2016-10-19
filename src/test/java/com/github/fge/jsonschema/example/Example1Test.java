b434d0252d1b9cbca505d1a6d4efeb9939d890e2
jackiezjiang <jackiezjiang@gmail.com>
2016-10-19 10:18:09 -0400

    Update Example1Test.java

--- src/test/java/com/github/fge/jsonschema/example/Example1Test.java
+++ src/test/java/com/github/fge/jsonschema/example/Example1Test.java
@@ -8,8 +8,7 @@ public  class Example1Test
 
  public static void main(String[] args)
         throws IOException, ProcessingException {
-         Method method = Example1.class.getMethod("main",  String[].class); 
-         method.invoke(null, new Object[]{new String[]{}});
+         Example1.class.getMethod("main",  String[].class).invoke(null, new Object[]{new String[]{}});
            
         }
 }
