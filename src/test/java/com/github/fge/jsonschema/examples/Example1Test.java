43a1555aa6539f58b17f80d05df42a89154bc675
jackiezjiang <jackiezjiang@gmail.com>
2016-10-19 23:23:27 -0400

    Create Example1Test.java

---
+++ src/test/java/com/github/fge/jsonschema/examples/Example1Test.java
@@ -0,0 +1,17 @@
+package com.github.fge.jsonschema.examples;
+
+
+import java.io.IOException;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+
+public  class Example1Test
+
+{
+
+ public static void main(String[] args)
+        throws IOException, ProcessingException {
+        // Example1.class.getMethod("main",  String[].class).invoke(null, new Object[]{new String[]{}});
+     Example1.main(null);
+        }
+}
+
