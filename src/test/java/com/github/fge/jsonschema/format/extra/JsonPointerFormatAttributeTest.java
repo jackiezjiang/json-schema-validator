9c18dd8cdd6c448385c9af962dd36c908ed1d506
jackiezjiang <jackiezjiang@gmail.com>
2016-10-19 23:33:25 -0400

    Create JsonPointerFormatAttributeTest.java

---
+++ src/test/java/com/github/fge/jsonschema/format/extra/JsonPointerFormatAttributeTest.java
@@ -0,0 +1,16 @@
+package com.github.fge.jsonschema.format.extra;
+
+import java.io.IOException;
+
+/**
+ * Created by Jackie on 10/19/16.
+ */
+public final class JsonPointerFormatAttributeTest extends ExtraFormatAttributeTest
+    {
+    public JsonPointerFormatAttributeTest()
+        throws IOException
+        {
+            super("json-pointer");
+        }
+
+}
